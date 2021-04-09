63aa7e53e15be12628055980d1c75ac8b42a6231
jgverges <jgverges@yahoo.es>
2021-04-09 13:33:54 +0200

    afterEach

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,13 +16,25 @@ describe('AppComponent', async () => {
     })
     .createComponent(AppComponent);
   });
+  afterEach(async () => {
+    (<HTMLInputElement>document.getElementById('num1')).value='0';
+    (<HTMLInputElement>document.getElementById('num2')).value='0';
+    document.getElementById('calc').click();
+  })
 
   it('suma debe dar 4', async () => {
     (<HTMLInputElement>document.getElementById('num1')).value='2';
     (<HTMLInputElement>document.getElementById('num2')).value='2';
     document.getElementById('calc').click();
-    console.log('***',(<HTMLInputElement>document.getElementById('result')).value);
     expect((<HTMLInputElement>document.getElementById('result')).value).toBe('4');
+
+  })
+
+  it('suma debe dar 10', async () => {
+    (<HTMLInputElement>document.getElementById('num1')).value='5';
+    (<HTMLInputElement>document.getElementById('num2')).value='5';
+    document.getElementById('calc').click();
+    expect((<HTMLInputElement>document.getElementById('result')).value).toBe('10');
 
   })
 })
